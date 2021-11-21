e8c29b1c71e300cffe1cd0b1ff1c4cb5140eca97
Szymekkkk <57588597+Szymekkkk@users.noreply.github.com>
2021-11-21 01:10:19 +0100

    nie pamietam nawet

--- Lista3/app/src/main/java/com/example/lista3/MainActivity.java
+++ Lista3/app/src/main/java/com/example/lista3/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.lista3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
     public void ButtonClick(View view)
     {
@@ -27,8 +29,22 @@ public class MainActivity extends AppCompatActivity {
                 Intent Test = new Intent(MainActivity.this, Test.class);
                 startActivity(Test);
                 break;
+            case "Zaloguj":
+                pref(view);
+                break;
+
             default:
                 break;
         }
+    }
+    public void pref(View view){
+        EditText login = findViewById(R.id.editTextTextPersonName);
+        String logon = login.getText().toString();
+        SharedPreferences preferences = getSharedPreferences("Prefs",MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString(logon,logon);
+        editor.commit();
     }
 }
